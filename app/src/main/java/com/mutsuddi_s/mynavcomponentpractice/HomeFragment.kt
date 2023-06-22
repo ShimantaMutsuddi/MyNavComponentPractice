35488b7eab459a2392c88914c5ce81befb13a380
Shimanta Mutsuddi <Mutsuddi005@gmail.com>
2023-06-22 07:09:13 +0600

    ...

--- app/src/main/java/com/mutsuddi_s/mynavcomponentpractice/HomeFragment.kt
+++ app/src/main/java/com/mutsuddi_s/mynavcomponentpractice/HomeFragment.kt
@@ -5,6 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.mutsuddi_s.mynavcomponentpractice.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment(R.layout.fragment_home)
+{
+    // assign the _binding variable initially to null and
+    // also when the view is destroyed again it has to be set to null
+    private var _binding: FragmentHomeBinding? = null
+
+    // with the backing property of the kotlin we extract
+    // the non null value of the _binding
+    private val binding get() = _binding!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController=findNavController()
+
+        binding.btnViewBalance.setOnClickListener {
+            navController.navigate(R.id.viewBalanceFragment)
+
+        }
+        binding.btnViewTransection.setOnClickListener {
+            navController.navigate(R.id.viewTransectionFragment)
+
+        }
+        binding.btnSendMoney.setOnClickListener {
+            navController.navigate(R.id.chooseReceiverFragment)
+
+        }
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
